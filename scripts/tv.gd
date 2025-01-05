c067348457b90b89180d6355daacda73b93b651d
WildBunnie <wildbunnie2003@gmail.com>
2025-01-05 14:02:54 +0000

    tv vhs shader

--- scripts/tv.gd
+++ scripts/tv.gd
@@ -4,12 +4,17 @@ extends StaticBody3D
 @onready var player: CharacterBody3D = $"../Player"
 @onready var blockbench_export: Node3D = $blockbench_export
 @onready var center: Node3D = $center
+@onready var camera_feed: Sprite3D = $CameraFeed2
 
 var current_rotation = 0
 var camera_distance = 5.0  # Distance from the object to the camera
 var player_distance = 1.0
 var rotation_speed = 0.01  # Speed of rotation
 var locked = false
+
+var pos1 = Vector3(-4.626012, 1.75, -1.897371)
+var pos2 = Vector3(-0.30752, 1.75, -4.990534) 
+var pos3 = Vector3(-1.721765, 1.75, -4.694201)
 
 var frustum_max
 var frustum_min
@@ -30,8 +35,29 @@ func interact(caller):
 		locked = false
 		caller.toggle_tv_label()
 
+func change_camera_noise():
+	# Calculate distances between the camera and the predefined positions
+	var distance_to_pos1 = camera_3d.global_transform.origin.distance_to(pos1)
+	var distance_to_pos2 = camera_3d.global_transform.origin.distance_to(pos2)
+	var distance_to_pos3 = camera_3d.global_transform.origin.distance_to(pos3)
+
+	# Find the closest position to the camera
+	var min_distance = min(distance_to_pos1, distance_to_pos2, distance_to_pos3)
+
+	# Calculate the noise intensity based on the closest distance
+	var noise_intensity = min_distance * 0.01  # Scale the distance to get an intensity range between 0 and 0.03
+
+	# Clamp the value to ensure it stays within the range [0, 0.03]
+	noise_intensity = clamp(noise_intensity, 0, 0.01)
+
+	# Set the shader parameter for noise intensity
+	camera_feed.material_override.set_shader_parameter("noiseIntensity", noise_intensity)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	
+	change_camera_noise()
+
 	if locked:
 		if Input.is_action_pressed("move_left"):
 			rotation.y -= rotation_speed
@@ -39,12 +65,6 @@ func _physics_process(delta: float) -> void:
 		elif Input.is_action_pressed("move_right"):
 			rotation.y += rotation_speed
 			blockbench_export.rotation.y -= rotation_speed
-			
-	#if Input.is_action_just_pressed("interact"):
-		#if not locked:
-			#locked = true
-		#else:
-			#locked = false
 
 	if locked:
 		if Input.is_action_just_released("extra_1"):
